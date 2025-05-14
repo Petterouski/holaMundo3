7d702ca0802cdc8606c5daf6b1f882f985d9c484
Petterouski <petterrp2002@hotmail.com>
2025-05-13 22:09:24 -0500

    boton regresar front
    se agregó un botón que regresa al front

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import os
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def hola_mundo():
-    return "<h1>Hola Mundo desde HEROKU</h1>"
+    return '''
+        <h1>Hola Mundo desde HEROKU</h1>
+        <button onclick="window.location.href='https://front-paas-production.up.railway.app/ '">
+            Regresar
+        </button>
+    '''
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
